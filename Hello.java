00dc782547517df6b0847b5120cd413aa2f7f664
reppoo <49638559+reppoo@users.noreply.github.com>
2019-04-15 12:37:45 +0300

    add hello.java
    пусть будет

---
+++ Hello.java
@@ -0,0 +1,35 @@
+//import java.util.*;
+class Hello{
+public static void main(String[] args){
+Hello Hello1 = new Hello();
+System.out.println("How many times to display a message?");
+
+}
+}
+//Scanner in = new Scanner(System.in);
+//int a = in.nextInt();
+//for (int i = 0; i<a; i++){
+	//System.out.println("Hello, World!");
+//}
+
+//System.out.println("What do you like?");
+//String likes="";
+//Scanner ni = new Scanner(System.in);
+//String s = null;
+//System.out.println("Start "+s);
+//s = in.nextLine();
+//System.out.println("Start "+s);
+
+
+/*for(;;){
+	
+	String s = ni.nextLine();
+	if(s!="\n"){
+		likes+=s;
+	System.out.println("You like: "+likes+".");
+	System.out.println("Anything else?");
+	s = "";
+	}
+	else break;
+}*/
+
