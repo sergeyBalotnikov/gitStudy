5b776164ae47427a8cb2f4281ff3ad50130a16de
sergeyBalotnikov <sergey.balotnikov@gmail.com>
2019-04-15 20:51:41 +0300

    compare

--- lesson3/module1/src/my/auto/Car.java
+++ lesson3/module1/src/my/auto/Car.java
@@ -3,6 +3,8 @@ package my.auto;
 public class Car {
 
     int mileage = 0;
+    boolean isServiseNead;
+    int gas = 0;
     public void driveForward(){
         mileage++;
         System.out.println("Mileage = "+mileage);
@@ -10,5 +12,13 @@ public class Car {
     public void driveBackward(){
         mileage--;
         System.out.println("Mileage = "+mileage);
+    }
+    public void checkServise(){
+        String message;
+        message = isServiseNead ? "Please go to car service": "No need service";
+        System.out.println(message);
+    }
+    public void checkGasTank(){
+        System.out.println("Gas: " + gas + " li.");
     }
 }
--- lesson3/module1/src/my/auto/Main.java
+++ lesson3/module1/src/my/auto/Main.java
@@ -19,9 +19,24 @@ public class Main {
         System.out.println(15==10);
         String s1 = "s1";
         String s2 = "s1";
-        System.out.println("s1==s2"+(s1==s2));
-        System.out.println("s1.equals(s2)"+(s1.equals(s2)));
+        System.out.println("s1==s2 "+(s1==s2));
+        System.out.println("s1.equals(s2) "+(s1.equals(s2)));
 
+        car1.checkServise();
+        car1.isServiseNead=true;
+        car1.checkServise();
+        car1.checkGasTank();
+        car1.gas+=40;
+        car1.checkGasTank();
+        car1.gas-=10;
+        car1.checkGasTank();
+        int i2 = 0;
+        i = 0;
+        //i2= i;
+        i2 = i++;
+        //i=i+1;
+        System.out.println(i);
+        System.out.println(i2);
 
     }
 }
