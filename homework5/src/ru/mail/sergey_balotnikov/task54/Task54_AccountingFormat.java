7f8f8ce6539e547f1b6fd55aaec3ce47e8bd7023
sergeyBalotnikov <sergey.balotnikov@gmail.com>
2019-05-05 14:51:29 +0300

    homuvork 5. Task 54 (new)

--- homework5/src/ru/mail/sergey_balotnikov/task54/Task54_AccountingFormat.java
+++ homework5/src/ru/mail/sergey_balotnikov/task54/Task54_AccountingFormat.java
@@ -1,22 +1,32 @@
 package ru.mail.sergey_balotnikov.task54;
 
+import java.lang.reflect.Array;
 import java.util.Scanner;
 
 public class Task54_AccountingFormat {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter number: ");
-        String Number = in.nextLine();
-        int number = Integer.parseInt(Number);
-        int [] accountingFormat = new int[Number.length()];
-        for (int i = accountingFormat.length-1; i >=0; i--){
-            accountingFormat[i] = number%10;
-            number/=10;
-        }
-        for (int i = 0; i<accountingFormat.length;i++){
-        if((accountingFormat.length-i)%3==0&&i!=0)System.out.print(" ");
-        System.out.print(accountingFormat[i]);
+        long number;
+        String inputValue;
+        while(true){
+            System.out.println("Enter number: ");
+            inputValue = in.nextLine();
+            try {
+                number = Long.parseLong(inputValue);
+                break;
+            } catch (Exception e){
+                System.out.println("Input wrong value. Try again");
+            }
         }
 
+        char [] inputValToChar = inputValue.toCharArray();
+        for(int i = 0; i<inputValToChar.length; i++){
+            System.out.print(inputValToChar[i]);
+            if((inputValToChar.length-i-1)%3 == 0 && inputValToChar[i]!='-'){
+                System.out.print(" ");
+            }
+        }
     }
+
 }
